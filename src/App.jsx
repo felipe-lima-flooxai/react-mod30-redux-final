8c01eba20ba0954cd71be09f3959050c2e4371ce
felipe-lima-flooxai <felipe.lima.flooxai@gmail.com>
2025-05-16 14:01:00 -0300

    feat: header do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import MainLayout from './Componentes/MainLayout'
+import Header from './Componentes/Header'
 import './App.css'
 
 function App() {
@@ -8,10 +9,7 @@ function App() {
   return (
     <>
       <MainLayout>
-        <header>
-          <h1>Contador Global</h1>
-          <p>Este projeto usa Redux para gerenciar o estado global do contador.</p>
-        </header>
+        <Header></Header>
 
         <section>
           {/* Aqui virá o card do contador */}
---
+++ src/Componentes/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Contador Global</h1>
+      <p>Este projeto usa Redux para gerenciar o estado global do contador.</p>
+    </header>
+  );
+}
+
+export default Header;
